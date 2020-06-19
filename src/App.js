403dc88fdcdc689f6311060919c35fb92f6be410
NewApps <rakeshkartikecse@gmail.com>
2020-06-19 21:26:38 +0530

    Extra files is removed from Food Recipe Application

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header";
+import Recipes from "./components/Recipes"
 
 function App() {
-  return (
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>food recipe</h1>
+      <Header />
+      <Recipes />
+
     </div>
-  );
+  )
 }
 
 export default App;
